c0ad047480092743ef67fa4f3c0a7d102c45a629
gilman qazi <gilmanqazi@gmail.com>
2025-10-13 10:22:07 +0530

    react

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+<>
+     <div id="parent">
+         <h1 id="child1">gilman</h1>
+         <h2 id="child2">gilman</h2>
+    </div>
+ <div id="parent2">
+  <h1>qazi</h1>
+  <h2>qazi</h2>
+ </div>   
+ 
+   </>
+  );
+};
+
+export default App;
